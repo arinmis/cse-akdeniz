41e7a46f628f793132e62b5b535180ee678c2f6b
arinmis <mustafa_arinmis@outlook.com>
2023-05-08 01:22:36 +0300

    Add JSON endpoint

--- spring-2023/seminar/gRPC-presentation/demo/node-service/index.js
+++ spring-2023/seminar/gRPC-presentation/demo/node-service/index.js
@@ -1,3 +1,18 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/json', (req, res) => {
+  const generated = generateData();
+  res.json(generated)
+})
+
+app.listen(port, () => {
+  console.log(`Node service is listening on port ${port}`)
+})
+
+/* generate random data object */ 
 function generateData() { 
   return ({
     time: Date.now(),
