ee4d3f0d520f2d9d41f428a454590eeeadb1fe5c
arinmis <mustafa_arinmis@outlook.com>
2023-05-18 21:09:30 +0300

    Read csv and build graph

--- spring-2023/artificial-intelligence/homeworks/project-1/submission.py
+++ spring-2023/artificial-intelligence/homeworks/project-1/submission.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from queue import PriorityQueue
 
 
@@ -9,7 +10,13 @@ class CityNotFoundError(Exception):
 
 # Implement this function to read data into an appropriate data structure.
 def build_graph(path):
-    pass
+    graph = {}
+    with open(path, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader) # skip first row
+        for row in reader:
+            graph[row[0]] = {row[1]: int(row[2])}
+    return graph
 
 
 # Implement this function to perform uniform cost search on the graph.
@@ -19,4 +26,18 @@ def uniform_cost_search(graph, start, end):
 
 # Implement main to call functions with appropriate try-except blocks
 if __name__ == "__main__":
-    pass
+
+    is_file_read = False
+    while not is_file_read:  
+        road_map_path = input("enter road map file path: ").strip()
+        print("hrere", road_map_path)
+        try:
+           graph = build_graph(road_map_path)
+           is_file_read = True
+        except :
+            print("file not found:", road_map_path)
+    print("graph: ", graph)
+
+    starting_city = input("Enter starting city: ")
+    destination_city = input("Enter destination city: ")
+    print("road map is loading...")
