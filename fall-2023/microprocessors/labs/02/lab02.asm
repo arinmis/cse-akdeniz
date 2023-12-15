3e8de8b0567259db706014bb8eaefe8763b0bc61
Mustafa Arinmis <mustafa.arinmis@piworks.net>
2023-12-15 20:09:58 +0300

    add micro lab 02

---
+++ fall-2023/microprocessors/labs/02/lab02.asm
@@ -0,0 +1,26 @@
+org 100h
+
+; set SI 2000h
+MOV SI, 2000h
+        
+MOV CX, N ; loop counter
+MOV AX, 0 ; store total sum
+MOV BX, 0 ; value to assign      
+MOV DX, 1 ; use BX as counter      
+
+loopstarts:            
+    ; run this code N times           
+    MOV [SI + BX], DX  ; assign value to memory 
+    ADD AX, DX ; add to total 
+    INC BX ; go to next address
+    INC DX ; increment counter 
+loop loopstarts
+
+MOV [SI + BX], AX  ; assign value to memory    
+
+ret
+   
+N DW 5
+
+end           
+      
