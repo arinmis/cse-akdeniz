78fe74a965a7035f43fcd4f5b3228e60fb8f7508
Mustafa Arinmis <mustafa.arinmis@piworks.net>
2023-12-29 00:01:52 +0300

    implement heap sort

--- fall-2023/algorithms/sorting/heapsort/heapsort.py
+++ fall-2023/algorithms/sorting/heapsort/heapsort.py
@@ -1,27 +1,35 @@
 def heapify(A, i, n):
     largest = i
-    if 2 * i <= n and A[2 * i + 1] > A[i]:
+    left_index =  2 * i + 1
+    right_index =  2 * i + 2
+    if left_index <= n and A[left_index] > A[i]:
         largest = 2 * i + 1
-    if 2 * i + 1 <= n and A[2 * i + 2] > A[largest]:
+    if right_index <= n and A[right_index] > A[largest]:
         largest = 2 * i + 2
     if not largest == i:
         A[i], A[largest] = A[largest], A[i]
         heapify(A, largest, n)
 
 def extract_max(A, n):
-    pass
+    max_value = A[0]
+    A[0] = A[n]
+    n -= 1
+    heapify(A, 0, n)
+    return max_value
 
 def build_heap(A, n):
     pass
 
 def heapsort(A, n):
-    pass
+    for i in range(n, 0, -1):
+        A[0], A[i] = A[i], A[0]
+        heapify(A, 0, i - 1)
+        print(i - 1)
+        print(A)
 
-A = [1, 14, 10, 8, 7, 9, 3, 2, 4, 16]
+A = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+n = len(A) - 1
 print(A)
-heapify(A, 0, len(A) - 2)
+print(heapsort(A, n))
 print(A)
-
-
-
 
