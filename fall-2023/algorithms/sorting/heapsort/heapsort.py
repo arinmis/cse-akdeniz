71e01fc66a5097b5c681f8165652a5f5a0401816
Mustafa Arinmis <mustafa.arinmis@piworks.net>
2023-12-29 13:43:40 +0300

    implement build heap helper

--- fall-2023/algorithms/sorting/heapsort/heapsort.py
+++ fall-2023/algorithms/sorting/heapsort/heapsort.py
@@ -18,18 +18,19 @@ def extract_max(A, n):
     return max_value
 
 def build_heap(A, n):
-    pass
+    for i in range(n // 2, -1, -1):
+        heapify(A, i, n)
 
 def heapsort(A, n):
+    build_heap(A, n)
     for i in range(n, 0, -1):
         A[0], A[i] = A[i], A[0]
         heapify(A, 0, i - 1)
-        print(i - 1)
-        print(A)
 
-A = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+# A = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+A = [4, 1, 3, 2, 7, 9, 10, 14, 8, 16]
 n = len(A) - 1
 print(A)
-print(heapsort(A, n))
+heapsort(A, n)
 print(A)
 
